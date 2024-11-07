24e1fd328042b9d4a4c8cad098e332d089a8ecac
Manu G <manusvathgurudath@gmail.com>
2024-11-07 00:27:18 -0800

    W

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300..700&display=swap"
           rel="stylesheet"
         />
+        <link rel="favicon" href="/favicon.ico" />
       </head>
       <body className={`antialiased`}>{children}</body>
     </html>
