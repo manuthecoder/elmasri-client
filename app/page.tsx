564335f2e679645013a24ffdb9c11fdb96ef5907
Manu G <manusvathgurudath@gmail.com>
2024-11-07 18:44:17 -0800

    W

--- app/page.tsx
+++ app/page.tsx
@@ -141,65 +141,65 @@ function HowWasThisCreated() {
           How was this project created?
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[425px] ">
         <DialogHeader>
           <DialogTitle>A bit about this project...</DialogTitle>
-          <DialogDescription>
-            Hey there, I'm Manu, the person behind this fun project. As a
-            student who finds AP physics challenging, I built this tool to help
-            students like me
-            <br />
-            <Button
-              className="mt-2 mb-5"
-              style={{
-                backgroundColor: "#0077B5",
-                color: "#fff",
-              }}
-              onClick={() => window.open("https://click.dysperse.com/KUkaLrn")}
-            >
-              LinkedIn
-            </Button>
-            <br />
-            <span className="text-black dark:text-white mb-0 font-bold">
-              Other projects?
-            </span>
-            <br />
-            <span>
-              Check out other cool stuff I've built on{" "}
-              <a
-                href="https://click.dysperse.com/XpY4XBi"
-                target="_blank"
-                className="underline"
-              >
-                my website
-              </a>
-            </span>
-            <br />
-            <br />
-            <span className="text-black dark:text-white mb-0 font-bold">
-              How does this project run?
-            </span>
-            <br />
-            <span>
-              This project is built using Next.js, Vercel, and a custom AI
-              infrastructure backend. The AI model is built using Google's
-              Gemini model and is fine-tuned on AP Physics content.
-            </span>
-            <br />
-            <br />
-            <span className="text-black dark:text-white mb-0 font-bold">
-              How's this any different from ChatGPT?
-            </span>
-            <br />
-            <span>
-              ChatGPT is a general-purpose conversational AI model. This model
-              is specifically trained on AP Physics content and is more likely
-              to provide accurate and relevant responses to your questions. It's
-              also trained using the Official CollegeBoard AP Physics Course and
-              Exam Descriptions, Lab Manuals, and other resources.
-            </span>
-          </DialogDescription>
         </DialogHeader>
+        <DialogDescription>
+          Hey there, I'm Manu, the person behind this fun project. As a student
+          who finds AP physics challenging, I built this tool to help students
+          like me
+          <br />
+          <Button
+            className="mt-2 mb-5"
+            style={{
+              backgroundColor: "#0077B5",
+              color: "#fff",
+            }}
+            onClick={() => window.open("https://click.dysperse.com/KUkaLrn")}
+          >
+            LinkedIn
+          </Button>
+          <br />
+          <span className="text-black dark:text-white mb-0 font-bold">
+            Other projects?
+          </span>
+          <br />
+          <span>
+            Check out other cool stuff I've built on{" "}
+            <a
+              href="https://click.dysperse.com/XpY4XBi"
+              target="_blank"
+              className="underline"
+            >
+              my website
+            </a>
+          </span>
+          <br />
+          <br />
+          <span className="text-black dark:text-white mb-0 font-bold">
+            How does this project run?
+          </span>
+          <br />
+          <span>
+            This project is built using Next.js, Vercel, and a custom AI
+            infrastructure backend. The AI model is built using Google's Gemini
+            model and is fine-tuned on AP Physics content.
+          </span>
+          <br />
+          <br />
+          <span className="text-black dark:text-white mb-0 font-bold">
+            How's this any different from ChatGPT?
+          </span>
+          <br />
+          <span>
+            ChatGPT is a general-purpose conversational AI model. This model is
+            specifically trained on AP Physics content and is more likely to
+            provide accurate and relevant responses to your questions. It's also
+            trained using the Official CollegeBoard AP Physics Course and Exam
+            Descriptions, Lab Manuals, and other resources.
+          </span>
+        </DialogDescription>
       </DialogContent>
     </Dialog>
   );
