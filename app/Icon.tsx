984e8b540dc54a370670fa9f986b7eb5174fca20
Manu G <manusvathgurudath@gmail.com>
2024-11-23 10:12:37 -0800

    W

--- app/Icon.tsx
+++ app/Icon.tsx
@@ -1,17 +1,21 @@
-export const Icon = ({
-  children,
-  className,
-  style = {},
-}: {
-  children: any;
-  className?: string;
-  style?: any;
-}) => (
-  <span
-    className={`material-symbols-rounded ${className} select-none`}
-    style={{ lineHeight: "15px", ...style }}
-  >
-    {children}
-  </span>
+import { memo } from "react";
+
+export const Icon = memo(
+  ({
+    children,
+    className,
+    style = {},
+  }: {
+    children: any;
+    className?: string;
+    style?: any;
+  }) => (
+    <span
+      className={`material-symbols-rounded ${className} select-none`}
+      style={{ lineHeight: "15px", ...style }}
+    >
+      {children}
+    </span>
+  )
 );
 
